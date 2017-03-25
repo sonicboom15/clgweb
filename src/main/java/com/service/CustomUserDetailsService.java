bb00ec89e5b2d846e4b9c40c8cbe1bec1f694029
Rishi Kumar <rishikumar15@gmail.com>
2017-03-25 13:56:04 +0530

    Completed Security Configurations(May add more details later)

---
+++ src/main/java/com/service/CustomUserDetailsService.java
@@ -0,0 +1,9 @@
+package com.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service("customeUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService{
+	
+}
